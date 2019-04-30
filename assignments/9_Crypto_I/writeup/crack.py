0d7ac0ae6751fe715fc8dbadd015b4ee2fb72bf6
akshayanil24 <akshayanil24@gmail.com>
2019-04-29 22:09:05 -0400

    completed assigment 9

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,14 +4,16 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open("hashes.txt").read().splitlines()
+    passwords = open("passwords.txt").read().splitlines()
     characters = string.ascii_lowercase
 
     for c in characters:
         for p in passwords:
-            # crack hashes
+            hash = hashlib.sha256(c+p).hexdigest()
 
+            if hash in hashes:
+                print(p + ":" + hash)
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
 
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,17 +6,27 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open("hashes.txt").read().splitlines()
+    passwords = open("passwords.txt").read().splitlines()
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
+
+    for i in range(3):
+        data = s.recv(1024)
+        prompt = data.splitlines()[2]
+        for c in characters:
+            for p in passwords:
+                #time.sleep(1)
+                hash = hashlib.sha256(c+p).hexdigest()
+                if hash == prompt:
+                    #print(p + ":" + hash)
+                    s.send(c+p+'\n')
     # parse data
     # crack 3 times
-
+    print(s.recv(1024))
 if __name__ == "__main__":
     server_crack()
